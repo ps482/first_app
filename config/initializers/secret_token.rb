491c62eed9ceaf83ba43c3006ad948b7f5c7ea8a
Vladimir <post.scriptum.ua@gmail.com>
2012-01-12 16:50:01 +0200

    RREADME deleted

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '2bd1407015db9d0469ec398803e81b0d080d0224c99c3ad69bb3c103c217111923b85c7989fffe9f298590149cb33ea870c628987f7c45e5e13b48e5b5076d09'
